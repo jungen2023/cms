2654091f8f6f346a11fbaf6e1c646ee1b0826ca5
zhaoya <12462303+jungen2023@user.noreply.gitee.com>
2025-04-05 23:58:39 +0800

    dockerfile

--- dockfile/dockerfile-seacms/alpine-mariadb/nfs-client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#挂载磁盘
-echo "nfs-server:/nfsshare/seacms/mdb /var/lib/mysql nfs ro,noatime,nofail,_netdev 0 0" >> /etc/fstab
-mount -a
--- dockfile/dockerfile-seacms/alpine-php5-nginx/nfs-client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#挂载磁盘
-echo "nfs-server:/nfsshare/seacms/app /var/www/localhost/htdocs nfs ro,noatime,nofail,_netdev 0 0" >> /etc/fstab
-mount -a
